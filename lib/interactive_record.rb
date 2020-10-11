4e736fd5b548039b0acd2e944323f6e68eee0f5f
J K <jkim65@buffalo.edu>
2020-10-11 21:49:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,7 +35,7 @@ class InteractiveRecord
 
 
   def col_names_for_insert
-    self.class.column_names.delete_if{|col| col == "id"}.join(",")
+    self.class.column_names.delete_if{|col| col == "id"}.join(", ")
   end
 
 end
